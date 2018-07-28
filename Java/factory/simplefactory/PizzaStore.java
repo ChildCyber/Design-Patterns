1f84d9b36bb1ccb70d4cf0eea17059a382772137
Mr.Child <lishunsheng2012@gmail.com>
2018-07-28 14:38:43 +0900

    add simple factory

---
+++ Java/factory/simplefactory/PizzaStore.java
@@ -0,0 +1,21 @@
+public class PizzaStore {
+    SimplePizzaFactory factory;
+
+    // 构造器，需要一个工厂作为参数
+    public PizzaStore(SimplePizzaFactory factory) {
+        this.factory = factory;
+    }
+
+    public Pizza orderPizza(String type) {
+        Pizza pizza;
+
+        pizza = factory.createPizza(type);
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+
+        return pizza;
+    }
+}
